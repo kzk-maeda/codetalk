2b63f9101a20634fae130652bca13804231039cb
kzk-maeda <casino.music0711@gmail.com>
2020-07-23 16:50:02 +0900

    route changed scope -> namespace

--- codetalk_api/config/routes.rb
+++ codetalk_api/config/routes.rb
@@ -3,11 +3,11 @@ require 'devise_token_auth'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
-    scope :v1 do
+    namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations'
       }
-      get "talks", :to=>"v1/talk/talks#index"
+      get "talks", :to=>"talk/talks#index"
     end
   end
 end
